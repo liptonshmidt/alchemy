65ee1ba2618b6be9bf096ba95cd6c9cdb27a9b9f
Alexey Lipnyagov <liptonshmidt@gmail.com>
2015-05-25 20:56:09 +0300

    Add simple navigation links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: "materials#index"
 
-  resources :materials, only: [:new, :create, :show]
+  resources :materials, only: [:new, :create, :show, :index]
 end
