bcbf03c0a1212194e8205d6ffe1a8600d060b4fe
Alexey Lipnyagov <liptonshmidt@gmail.com>
2015-06-02 00:26:01 +0300

    Set order for  user.materials assoc.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   include Clearance::User
 
-  has_many :materials
+  has_many :materials, -> { order(created_at: :desc) }
 
   def find_material(material_id)
     materials.find(material_id)
