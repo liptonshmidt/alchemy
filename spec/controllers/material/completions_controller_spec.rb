f1ed9f7c0bc4a29b1175a96bc1c8f1861bb9f35e
Alexey Lipnyagov <liptonshmidt@gmail.com>
2015-06-03 01:09:55 +0300

    Step 5. Pending controller specs

---
+++ spec/controllers/material/completions_controller_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+describe Material::CompletionsController, "#create" do
+  it "finds user's material"
+
+  it "sends :complete to material"
+
+  it "redirects to materials_path"
+end
