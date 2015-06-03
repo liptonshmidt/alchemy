1906b90d70e109a76398bd2ea8c213241fc830fb
Alexey Lipnyagov <liptonshmidt@gmail.com>
2015-06-03 11:38:03 +0300

    Step 13. Passing model, controller specs. Failing feature spec

--- app/controllers/material/completions_controller.rb
+++ app/controllers/material/completions_controller.rb
@@ -1,5 +1,15 @@
 class Material::CompletionsController < ApplicationController
+  before_action :require_login
+  before_action :find_material
+
   def create
+    @material.complete
     redirect_to materials_path
+  end
+
+  private
+
+  def find_material
+    @material = current_user.find_material params[:material_id]
   end
 end
